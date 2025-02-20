08e5844ab53df9d59c73117ae0316a518cb2655b
jeanramalho.dev <jeanramalho.dev@gmail.com>
2025-02-20 12:34:51 -0300

    [add] - Retira alamofire do projeto e refaz requisicao com o urlsession

--- ConsumindoMemesAPI/Controller/HomeViewController.swift
+++ ConsumindoMemesAPI/Controller/HomeViewController.swift
@@ -6,11 +6,12 @@
 //
 import Foundation
 import UIKit
-import Alamofire
 
 class HomeViewController: UIViewController {
     
     let contentView: HomeView
+    
+    var arrayMemes: [MemeObject] = []
     
     init(contentView: HomeView) {
         self.contentView = contentView
@@ -28,13 +29,11 @@ class HomeViewController: UIViewController {
     
     private func setup(){
         
-        AF.request("https://api.imgflip.com/get_memes").responseJSON { response in
-            
-        }
         
         contentView.memesTableView.delegate = self
         contentView.memesTableView.dataSource = self
         
+        fetchMemes()
         setHierarchy()
         setConstraints()
     }
@@ -53,16 +52,68 @@ class HomeViewController: UIViewController {
             contentView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
         ])
     }
+    
+    
+    func fetchMemes() {
+        // 1️⃣ Criamos a URL do endpoint da API
+        guard let url = URL(string: "https://api.imgflip.com/get_memes") else {
+            print("URL inválida")
+            return
+        }
+
+        // 2️⃣ Criamos uma URLSession para fazer a requisição
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            // 3️⃣ Verificamos se houve erro na requisição
+            if let error = error {
+                print("Erro na requisição:", error.localizedDescription)
+                return
+            }
+            
+            // 4️⃣ Garantimos que a resposta HTTP é válida e tem status 200 (sucesso)
+            if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 {
+                
+                // 5️⃣ Verificamos se os dados foram recebidos
+                guard let data = data else {
+                    print("Nenhum dado recebido")
+                    return
+                }
+                
+                do {
+                    // 6️⃣ Decodificamos os dados JSON no nosso modelo Meme
+                    let memeModel = try JSONDecoder().decode(Meme.self, from: data)
+                    
+                    // 7️⃣ Atualizamos a lista de memes na main thread para refletir na interface
+                    DispatchQueue.main.async {
+                        self.arrayMemes = memeModel.data.memes
+                        self.contentView.memesTableView.reloadData()
+                    }
+                    
+                } catch {
+                    // 8️⃣ Capturamos e exibimos erros de decodificação do JSON
+                    print("Erro ao decodificar JSON:", error.localizedDescription)
+                }
+            } else {
+                print("Resposta HTTP inválida ou status diferente de 200")
+            }
+        }
+        
+        // 9️⃣ Iniciamos a requisição
+        task.resume()
+    }
+    
 }
 
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return self.arrayMemes.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         guard let cell = tableView.dequeueReusableCell(withIdentifier: MemesTableViewCell.identifier, for: indexPath) as? MemesTableViewCell else {return UITableViewCell()}
+        
+        cell.memeTitleLabel.text = self.arrayMemes[indexPath.row].name
         
         return cell
     }
--- Pods/Target Support Files/Pods-ConsumindoMemesAPI/Pods-ConsumindoMemesAPI-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ConsumindoMemesAPI : NSObject
-@end
-@implementation PodsDummy_Pods_ConsumindoMemesAPI
-@end
